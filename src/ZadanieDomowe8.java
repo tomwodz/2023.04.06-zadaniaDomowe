3d506c6cb53ddca38a5345fabad38d9dc13be818
twodzinski@op.pl <twodzinski@op.pl>
2023-04-15 18:54:00 +0200

    Określenie warunków pętli - ich ogranicznie w celu maksymalizacji wydajności

--- src/ZadanieDomowe8.java
+++ src/ZadanieDomowe8.java
@@ -12,40 +12,46 @@ public class ZadanieDomowe8 {
         Arrays.fill(tabEra, false);
 
 
-       for (int i=0 ; i < tab.length ; i++) {
-           tab[i] = 2 + i;
-       }
-       System.out.println("Dane pogladowe: ");
-       System.out.println(Arrays.toString(tab));
-       System.out.println(Arrays.toString(tabEra));
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = 2 + i;
+        }
+        System.out.println("Dane pogladowe: ");
+        System.out.println(Arrays.toString(tab));
+        System.out.println(Arrays.toString(tabEra));
 
-        for (int i=0 ; i < tab.length ; i++) {
-            int temp;
-            for(int j = 2; (j <= 7) && ((j != 4) || (j != 6)); j++){
-                temp = tab[i]*j;
-              for (int k = 0; k < tab.length; k++){
-                  if(tab[k] == temp){
-                      tabEra[k] = true;
-                  };
-              }
+        int ilePetli = 0;
+
+        for (int i = 0 ; i < tab.length; i++) {
+            if(tabEra[i] != true)
+            {
+               for(int j = 2; tab[i] * j <= tab[tab.length-1] ; j++)
+                if(j!=4 || j!=6) {
+                    {
+                        for (int k = 0; k < tab.length; k++)
+                            if (tab[i] * j == tab[k]) {
+                                tabEra[k] = true;
+                            }
+                        ilePetli++;
+                    }
+                }
+            }
+        }
+
+            System.out.println(Arrays.toString(tabEra));
+            System.out.println(ilePetli);
+            System.out.println("----------------------------------- !!");
+            System.out.println("Wynik programu: ");
+
+            for (int i = 0; i < tab.length; i++) {
+                if (tabEra[i] == false) {
+                    System.out.print(tab[i] + " ");
                 }
             }
 
-
-        System.out.println(Arrays.toString(tabEra));
-
-        System.out.println("----------------------------------- !!");
-        System.out.println("Wynik programu: ");
-
-        for (int i=0 ; i < tab.length ; i++) {
-            if(tabEra[i] == false){
-                System.out.print(tab[i] + " ");
-            }
         }
-
     }
 
-        }
+
 
 
 
